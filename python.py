293bde48aa328b512f91793a9a09dce868821144
Nirnaya <basnetnirnaya7@gmail.com>
2025-10-28 18:24:38 +0545

    change

--- python.py
+++ python.py
@@ -1,7 +1,7 @@
 import random
 
 
-print("Enter level: \n 1)range 1-10 \n 2)range 1-50 \n 3) 1-100")
+print("Enter level: \n 1)range 1-10 \n 2)range 1-50 ")
 
 def lvl(min_num,max_num):
     ran= random.randint(min_num,max_num)
@@ -24,7 +24,6 @@ if r==1:
     lvl(1,10)
 elif r==2:
     lvl(1,50)
-elif r==3:
-    lvl(1,100)
+
 else:
     print("Invalid")
